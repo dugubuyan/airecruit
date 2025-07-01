787943145ee447d37ff5f1f73a512571257151db
Your Name <you@example.com>
2025-07-01 13:41:33 +0800

    filter

--- capacity/__init__.py
+++ capacity/__init__.py
@@ -1,7 +1,7 @@
 # from .optimize_resume import optimize_resume
 from .summarize_resume import summarize_resume
 from .generate_cover_letter import generate_cover_letter
-from .resume_filters import resume_to_sql_filters
+from .resume_filters import resume_filtering
 from .recommendation import generate_recommendation
 from .contact_extractor import extract_contact_and_send
 
@@ -9,7 +9,7 @@ __all__ = [
     # 'optimize_resume',
     'summarize_resume',
     'generate_cover_letter',
-    'resume_to_sql_filters',
+    'resume_filtering',
     'generate_recommendation',
     'extract_contact_and_send',
     'send_email'
